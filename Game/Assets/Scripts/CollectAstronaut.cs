8c7608c1c6fbff8d8df4f07052672c6285ed1a84
Abby <31456119+AbDaug@users.noreply.github.com>
2021-04-27 22:22:06 -0400

    Made astronauts collectable!
    Astronauts are now collectable! When the ship runs into them, they disappear/deactivate as objects. A counter increments each time one of them is collected. Major thing now is to add a UI element for the counter/win-screen at the end

---
+++ Game/Assets/Scripts/CollectAstronaut.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectAstronaut : MonoBehaviour
+{
+
+    private float counter = 0;
+
+    void Update()
+    {
+        //This is here for testing purposes. Doesn't do well for trying to test other things, but at least the counter works as intended
+        //Will not stick with Application.Quit(). Will change to enabling a UI element and disabling the game
+        //(showing a win-screen and letting the player exit organically)
+
+        /*if(counter == 8)  //probably should not hard-code this number (for future additions). works for now
+        {
+            Application.Quit();
+        }*/
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Collectable")
+        {
+            other.gameObject.SetActive(false);
+            counter++;
+            Debug.Log("Counter = " + counter);
+        }
+
+        //above works very well, astronauts disappear upon collision with ship
+        //counter above increments upon touch of a "collectable" tagged object
+        //now just need to translate the counter into a visual UI element and end the game once all astros are collected
+    }
+}
